b55b7ea71463c21103fa7d4a3ec577e67c16ecc3
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 07:09:24 -0800

    Add the Controller object to the main.py file.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import NoReturn
 from PySide6.QtWidgets import QApplication
 
 from helpers.helpers import get_ini_info
+from src.controller.controller import Controller
 from src.view.main_window import MainWindow
 
 
@@ -13,6 +14,7 @@ def run_app() -> NoReturn:
     com_port = ini['COM']
     app = QApplication([])
     window = MainWindow(version)
+    _ = Controller(com_port, window)
     window.show()
     sys.exit(app.exec())
 
