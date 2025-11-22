114a09439c9f40bacabc0cf9f954a2a40aaa52bc
fizziksfool <erbe.joshua@gmail.com>
2025-11-21 23:18:42 -0800

    Add the get_ini_info function to the helpers.py file.

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,9 +1,63 @@
+import configparser
 import sys
 from pathlib import Path
+from typing import TypeAlias
+
+ConfigData: TypeAlias = configparser.ConfigParser
 
 
 def get_root_dir() -> Path:
+    """
+    Get the root directory of the __main__ file.
+
+    Returns [str]:
+        Path object
+    """
     if getattr(sys, 'frozen', False):  # Check if running from the PyInstaller EXE
         return Path(getattr(sys, '_MEIPASS', '.'))
     else:  # Running in a normal Python environment
         return Path(__file__).resolve().parents[1]
+
+
+def load_config(file_name: str) -> ConfigData:
+    """
+    Get the data contained in the .ini file from file_name.
+
+    Inputs [file_name]:
+        Path to file_name
+    InputTypes [str]
+    Returns [configparser.ConfigParser]:
+        The parsed data contained in the .ini file.
+    """
+    config_data = configparser.ConfigParser()
+    config_data.read(file_name)
+    return config_data
+
+
+def get_ini_info(
+    config_dir: str = 'configuration',
+    ini_file: str = 'config.ini',
+) -> dict[str, str]:
+    """
+    Get the initialization information from the .ini file that is
+    in the configuration folder.
+
+    Inputs: [str, str]:
+        The name of the folder where the .ini file resides and the name of the .ini file.
+    Returns [dict(str, str)]:
+        The COM port listed in the ini file.
+    """
+    root_dir: Path = get_root_dir()
+    ini_file_path: str = str(root_dir / config_dir / ini_file)
+    config_data: ConfigData = load_config(ini_file_path)
+    com_port: str = config_data.get(section='COM_PORT', option='COM')
+
+    return {'COM': com_port}
+
+
+if __name__ == '__main__':
+    root_dir = get_root_dir()
+    ini = get_ini_info()
+
+    print(root_dir)
+    print(ini['COM'])
