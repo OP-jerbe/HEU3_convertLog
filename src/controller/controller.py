e6b28f069c2c5349a6133fdc626d29c9df39473f
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 07:28:24 -0800

    Add the Model to the Controller class __init__ method.

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -2,11 +2,14 @@ from PySide6.QtWidgets import QMainWindow
 
 from helpers.helpers import get_folder_path
 
+from ..model.model import Model
+
 
 class Controller:
-    def __init__(self, com_port: str, view: QMainWindow) -> None:
+    def __init__(self, com_port: str, view: QMainWindow, model: Model) -> None:
         self.com_port = com_port
         self.view = view
+        self.model = model
 
 
 if __name__ == '__main__':
