d9a043483414f1afea3d58c51355991562cdf72d
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 15:08:04 -0800

    Add the serial connection functionality to the main.py file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import NoReturn
 
 from PySide6.QtWidgets import QApplication
 
-from helpers.helpers import get_ini_info
+from helpers.helpers import connect_to_com_port, get_ini_info
 from src.controller.controller import Controller
 from src.model.model import Model
 from src.view.main_window import MainWindow
@@ -13,9 +13,14 @@ def run_app() -> NoReturn:
     version = '1.0.0'
     ini: dict[str, str] = get_ini_info()
     com_port = ini['COM']
-    app = QApplication([])
+    try:
+        ser = connect_to_com_port(com_port)
+    except Exception as e:
+        print(f'{str(e)}')
+        ser = None
+    app = QApplication(sys.argv)
     view = MainWindow(version)
-    model = Model()
+    model = Model(ser)
     _ = Controller(com_port, view, model)
     view.show()
     sys.exit(app.exec())
