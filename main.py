b88a7d34c83e7e30c8058025e61d272523af5c46
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 07:29:58 -0800

    Init Controller with a Model in the main.py file. Change the MainWindow variable to view.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import QApplication
 
 from helpers.helpers import get_ini_info
 from src.controller.controller import Controller
+from src.model.model import Model
 from src.view.main_window import MainWindow
 
 
@@ -13,9 +14,10 @@ def run_app() -> NoReturn:
     ini: dict[str, str] = get_ini_info()
     com_port = ini['COM']
     app = QApplication([])
-    window = MainWindow(version)
-    _ = Controller(com_port, window)
-    window.show()
+    view = MainWindow(version)
+    model = Model()
+    _ = Controller(com_port, view, model)
+    view.show()
     sys.exit(app.exec())
 
 
