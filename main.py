0c425c50ed76c6d5fd549f78a974895d4fe7be9a
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 06:48:16 -0800

    Add get_ini_info function to the main.py file.

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from typing import NoReturn
 
 from PySide6.QtWidgets import QApplication
 
+from helpers.helpers import get_ini_info
 from src.view.main_window import MainWindow
 
 
 def run_app() -> NoReturn:
-    version = "1.0.0"
+    version = '1.0.0'
+    ini: dict[str, str] = get_ini_info()
+    com_port = ini['COM']
     app = QApplication([])
     window = MainWindow(version)
     window.show()
     sys.exit(app.exec())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_app()
