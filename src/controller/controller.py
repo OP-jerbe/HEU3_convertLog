50da136b063edb9e2e1d59f15f8f1228ebc743d9
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 16:15:51 -0800

    Add Slots to controller.py. Change the view type from QMainWindow to MainWindow

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,15 +1,32 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Slot
 
 from helpers.helpers import get_folder_path
 
 from ..model.model import Model
+from ..view.main_window import MainWindow
 
 
 class Controller:
-    def __init__(self, com_port: str, view: QMainWindow, model: Model) -> None:
+    def __init__(self, com_port: str, view: MainWindow, model: Model) -> None:
         self.com_port = com_port
         self.view = view
         self.model = model
+
+        self.view.printIt_sig.connect(self.receive_printIt_sig)
+        self.view.csvIt_sig.connect(self.receive_csvIt_sig)
+        self.view.commandIt_sig.connect(self.receive_commandIt_sig)
+
+    @Slot(bool)
+    def receive_printIt_sig(self, signal: bool) -> None:
+        self.model.printIt = signal
+
+    @Slot(bool)
+    def receive_csvIt_sig(self, signal: bool) -> None:
+        self.model.csvIt = signal
+
+    @Slot()
+    def receive_commandIt_sig(self) -> None:
+        self.model.commandIt()
 
 
 if __name__ == '__main__':
