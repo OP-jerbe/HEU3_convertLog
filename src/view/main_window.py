1d82be54d9957d352e08dd5c0ca0367dbd0e3d58
fizziksfool <erbe.joshua@gmail.com>
2025-11-22 16:40:04 -0800

    Add the menubar and add the QLineEdits to the layout so they appear in the window.

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from PySide6.QtCore import Signal
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import (
     QCheckBox,
     QGridLayout,
@@ -9,6 +9,7 @@ from PySide6.QtWidgets import (
     QLabel,
     QLineEdit,
     QMainWindow,
+    QMenuBar,
     QPushButton,
     QVBoxLayout,
     QWidget,
@@ -43,23 +44,30 @@ class MainWindow(QMainWindow):
         apply_stylesheet(self, theme='dark_lightgreen.xml', invert_secondary=True)
         self.setStyleSheet(self.styleSheet() + """QLineEdit {color: lightgreen;}""")
 
+        self._create_menubar()
+
         self.printIt_cb = QCheckBox('Print Log')
         self.printIt_cb.setChecked(False)
         self.csvIt_cb = QCheckBox('Create CSV')
         self.csvIt_cb.setChecked(True)
         self.commandIt_pb = QPushButton('Pull Data Log')
-        self.SN_le = QLineEdit(placeholderText='Enter HEU Serial Number')
+        self.SN_le = QLineEdit(placeholderText='Enter Serial Number')
         self.logNum_le = QLineEdit(placeholderText='Enter Log Number')
 
         self.printIt_cb.clicked.connect(self.handle_printIt_clicked)
         self.csvIt_cb.clicked.connect(self.handle_csvIt_clicked)
         self.commandIt_pb.clicked.connect(self.handle_commandIt_clicked)
 
+        le_layout = QHBoxLayout()
+        le_layout.addWidget(self.SN_le)
+        le_layout.addWidget(self.logNum_le)
+
         cb_layout = QHBoxLayout()
         cb_layout.addWidget(self.printIt_cb)
         cb_layout.addWidget(self.csvIt_cb)
 
         main_layout = QVBoxLayout()
+        main_layout.addLayout(le_layout)
         main_layout.addLayout(cb_layout)
         main_layout.addWidget(self.commandIt_pb)
 
@@ -67,6 +75,15 @@ class MainWindow(QMainWindow):
         container.setLayout(main_layout)
 
         self.setCentralWidget(container)
+
+    def _create_menubar(self) -> None:
+        self.exit_action = QAction(text='Exit', parent=self)
+
+        self.menu_bar = self.menuBar()
+        self.file_menu = self.menu_bar.addMenu('File')
+        self.help_menu = self.menu_bar.addMenu('Help')
+
+        self.file_menu.addAction(self.exit_action)
 
     def handle_printIt_clicked(self) -> None:
         if self.printIt_cb.isChecked():
@@ -81,4 +98,6 @@ class MainWindow(QMainWindow):
             self.printIt_sig.emit(False)
 
     def handle_commandIt_clicked(self) -> None:
+        self.SN_changed_sig.emit(self.SN_le.text())
+        self.logNum_changed_sig.emit(self.logNum_le.text())
         self.commandIt_sig.emit()
